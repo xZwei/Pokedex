3048c3a8f215d21df02a64f010cd2a742b80edc4
Crash Override <wcole@outreach.com>
2021-06-12 16:16:35 -0600

    FEAT: pokemon type images (taken from serebii's website)

---
+++ data/pokemonTypeImages.js
@@ -0,0 +1,23 @@
+const baseSerebiiTypeImageUrl = "https://www.serebii.net/pokedex-bw/type/"
+
+const typeImages = {
+    bug: baseSerebiiTypeImageUrl + "bug.gif",
+    dark: baseSerebiiTypeImageUrl + "dark.gif",
+    dragon: baseSerebiiTypeImageUrl + "dragon.gif",
+    electric: baseSerebiiTypeImageUrl + "electric.gif",
+    fairy: baseSerebiiTypeImageUrl + "fairy.gif",
+    fighting: baseSerebiiTypeImageUrl + "fighting.gif",
+    fire: baseSerebiiTypeImageUrl + "fire.gif",
+    flying: baseSerebiiTypeImageUrl + "flying.gif",
+    ghost: baseSerebiiTypeImageUrl + "ghost.gif",
+    grass: baseSerebiiTypeImageUrl + "grass.gif",
+    ground: baseSerebiiTypeImageUrl + "ground.gif",
+    ice: baseSerebiiTypeImageUrl + "ice.gif",
+    normal: baseSerebiiTypeImageUrl + "normal.gif",
+    psychic:baseSerebiiTypeImageUrl + "psychic.gif",
+    poison: baseSerebiiTypeImageUrl + "poison.gif",
+    rock: baseSerebiiTypeImageUrl + "rock.gif",
+    steel: baseSerebiiTypeImageUrl + "steel.gif",
+}
+
+export default typeImages;
